21dbad9559137a7204b3565259a66275a010a336
Tahir Iqbal <tahir.iqbal@seventechnology.co.uk>
2018-11-08 13:32:39 +0500

    remove inline styles

--- src/component/Ludo.js
+++ src/component/Ludo.js
@@ -8,152 +8,61 @@ class Ludo extends Component {
           <div id="board" />
           <div id="dice" onclick="randomNum()" className="dice" />
           {/* <!-- red pawns  start top 28 left 318--> */}
-          <div
-            className="pawns"
-            id="redpawn1"
-            onclick="randomMove('red',1)"
-            style={{
-              "background-color": "#ff6969",
-              top: "149px",
-              left: "442px"
-            }}
-          />
-          <div
-            className="pawns"
-            id="redpawn2"
-            onclick="randomMove('red',2)"
-            style={{
-              "background-color": "#ff6969",
-              top: "102px",
-              left: "395px"
-            }}
-          />
-          <div
-            className="pawns"
-            id="redpawn3"
-            onclick="randomMove('red',3)"
-            style={{
-              "background-color": "#ff6969",
-              top: "55px",
-              left: "442px"
-            }}
-          />
-          <div
-            className="pawns"
-            id="redpawn4"
-            onclick="randomMove('red',4)"
-            style={{
-              "background-color": "#ff6969",
-              top: "102px",
-              left: "490px"
-            }}
-          />
+          <div className="pawns red redpawn1" onclick="randomMove('red',1)" />
+          <div className="pawns red redpawn2" onclick="randomMove('red',2)" />
+          <div className="pawns red redpawn3" onclick="randomMove('red',3)" />
+          <div className="pawns red redpawn4" onclick="randomMove('red',4)" />
           {/* <!-- yellow pawns  start top 523 left 218--> */}
           <div
-            className="pawns"
-            id="yellowpawn1"
+            className="pawns yellow yellowpawn1"
             onclick="randomMove('yellow',1)"
-            style={{
-              "background-color": "#c5c501",
-              top: "451px",
-              left: "47px"
-            }}
           />
           <div
-            className="pawns"
-            id="yellowpawn2"
+            className="pawns yellow yellowpawn2"
             onclick="randomMove('yellow',2)"
-            style={{
-              "background-color": "#c5c501",
-              top: "451px",
-              left: "140px"
-            }}
           />
           <div
-            className="pawns"
-            id="yellowpawn3"
+            className="pawns yellow yellowpawn3"
             onclick="randomMove('yellow',3)"
-            style={{
-              "background-color": "#c5c501",
-              top: "404px",
-              left: "93px"
-            }}
           />
           <div
-            className="pawns"
-            id="yellowpawn4"
+            className="pawns yellow yellowpawn4"
             onclick="randomMove('yellow',4)"
-            style={{
-              "background-color": "#c5c501",
-              top: "498px",
-              left: "93px"
-            }}
           />
           {/* <!-- green pawns  start top 227 left 22--> */}
           <div
-            className="pawns"
-            id="greenpawn1"
+            className="pawns green greenpawn1"
             onclick="randomMove('green',1)"
-            style={{ "background-color": "green", top: "149px", left: "93px" }}
+            style={{ top: "149px", left: "93px" }}
           />
           <div
-            className="pawns"
-            id="greenpawn2"
+            className="pawns green greenpawn2"
             onclick="randomMove('green',2)"
-            style={{ "background-color": "green", top: "102px", left: "140px" }}
           />
           <div
-            className="pawns"
-            id="greenpawn3"
+            className="pawns green greenpawn3"
             onclick="randomMove('green',3)"
-            style={{ "background-color": "green", top: "55px", left: "93px" }}
           />
           <div
-            className="pawns"
-            id="greenpawn4"
+            className="pawns green greenpawn4"
             onclick="randomMove('green',4)"
-            style={{ "background-color": "green", top: "102px", left: "47px" }}
           />
           {/* <!-- blue pawns  start top 326 left 515--> */}
           <div
-            className="pawns"
-            id="bluepawn1"
+            className="pawns blue bluepawn1"
             onclick="randomMove('blue',1)"
-            style={{
-              "background-color": "#6a6ac5",
-              top: "451px",
-              left: "490px"
-            }}
           />
           <div
-            className="pawns"
-            id="bluepawn2"
+            className="pawns blue bluepawn2"
             onclick="randomMove('blue',2)"
-            style={{
-              "background-color": "#6a6ac5",
-              top: "451px",
-              left: "395px"
-            }}
           />
           <div
-            className="pawns"
-            id="bluepawn3"
+            className="pawns blue bluepawn3"
             onclick="randomMove('blue',3)"
-            style={{
-              "background-color": "#6a6ac5",
-              top: "404px",
-              left: "442px"
-            }}
           />
           <div
-            className="pawns"
-            id="bluepawn4"
+            className="pawns blue bluepawn4"
             onclick="randomMove('blue',4)"
-            style={{
-              "background-color": "#6a6ac5",
-              top: "498px",
-              left: "442px"
-            }}
           />
           <h4 id="uselesstext1">It`s </h4>
           <h3 id="player" style={{ float: "left", color: "#ff6969" }}>
